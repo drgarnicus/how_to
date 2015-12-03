2652e516c8a2a549be7ce217e2bb0f2c9bde38c8
drgarnicus <ricroc628@aol.com>
2015-12-02 20:43:59 -0800

    Test that api works

---
+++ api_test.js
@@ -0,0 +1,12 @@
+apiKey = "533aa05d6b034a08a84de9ed8c8b598f";
+    		baseURL = "http://services.healthindicators.gov/v5/REST.svc/";
+    		api = new hiw.API(apiKey, baseURL);
+ 
+            logInfo("API Key: " + api.apiKey)
+ 
+            api.callMethod("/VerifyApiKey", null, function (r) {
+                if (r.Data == true)
+                    logInfo("Your API key is valid.");
+                else
+                    logError("Your API key is not valid.");
+            });
